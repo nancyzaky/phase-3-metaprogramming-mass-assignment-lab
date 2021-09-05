49d532922bf54bf50816099dae62464243408180
Nancy Zaky <nancyzki@yahoo.com>
2021-09-04 19:57:12 -0700

    commit tree b53009d96e36b74021e97a1bc3a0d58f29448982

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,30 @@
+require 'pry'
 class Person
   # your code here
+  def initialize(attrs)
+    attrs.each do |key , value|
+
+      self.class.attr_accessor(key)
+      self.send("#{key}=", value)
+    end
+
+  end
 end
+atr =    {
+        name: "Avi",
+        birthday: "01/29/1984",
+        hair_color: "brown",
+        eye_color: "brown",
+        height: "short",
+        weight: "good",
+        handed: "lefty",
+        complexion: "decent",
+        t_shirt_size: "medium",
+        wrist_size: "small",
+        glove_size: "normal",
+        pant_length: "32",
+        pant_width: "32"
+      }
+nancy = Person.new(atr)
+
+binding.pry
